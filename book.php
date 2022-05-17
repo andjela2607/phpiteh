d6e02e983a83e0570ad31c39279d1e5b358d8633
andjela2607 <andja011@gmail.com>
2022-05-17 19:18:33 +0200

    homepage

---
+++ book.php
@@ -0,0 +1,16 @@
+<?php
+    
+    include('components/db_config.php');
+
+    
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+    <?php include('components/header.php') ?>
+
+    <?php include('components/footer.php') ?>
+
+</html>
